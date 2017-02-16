98d0451300138d94ca118436e5d506e775c3a30c
xconstaud <xconstaud@gmail.com>
2017-02-16 09:59:36 -0600

    Adding New Exercises

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -14,4 +14,12 @@
     for (var i = 0; i < names.length; i++) {
     	console.log(names[i]);
     }
+
+    names.forEach(function (element, index, array) {
+    	console.log(element);
+    });
+
+
 })();
+
+
