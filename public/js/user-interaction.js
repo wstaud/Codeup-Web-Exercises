75e75153ad2d755e94fb0fdfbb6a17479d415f42
xconstaud <xconstaud@gmail.com>
2017-02-16 16:02:12 -0600

    note changes

--- public/js/user-interaction.js
+++ public/js/user-interaction.js
@@ -1,5 +1,7 @@
+//Declared Global Variables
 var userName = prompt("What is your name?");
 
+//Checking for null (cancels if so), if not trims blanks and checks for no input. Then continues.
 if (userName !== null) {
 	while (userName.trim() == "") {
 		userName = prompt("What is your name?");
@@ -8,7 +10,10 @@ if (userName !== null) {
 }else {
 	alert("That's okay, we don't need a name.");
 }
+//Declared Global Variables
 var pizzaAnswer = confirm("Do you like pizza?");
+
+//Checks true/false for Pizza input 
 if (pizzaAnswer == true) {
 	alert("Congrats, you are human.");
 }else {
