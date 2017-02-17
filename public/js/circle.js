89cb451c7d2c62d85b91dcb7311ad8453c4a4375
xconstaud <xconstaud@gmail.com>
2017-02-17 15:50:42 -0600

    Adding Exercises

--- public/js/circle.js
+++ public/js/circle.js
@@ -1,42 +1,33 @@
 (function() {
     "use strict";
 
-    // create a circle object
     var circle = {
+        //Set this raidus for the first output
         radius: 3,
 
         getArea: function () {
-            var area = (Math.PI * (circle.radius * circle.radius));
+            var area = (Math.PI * (this.radius * this.radius));
 
-            return area; // TODO: return the proper value
+            return area; 
         },
-
+        // redeclares radius locally and checks if it needs rounded or not
         logInfo: function (doRounding) {
-            // TODO: complete this method.
             var area = circle.getArea();
 
             if (doRounding == true) {
                 area = Math.round(area);
             }
-            
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
             console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
-
-    // log info about the circle
+    //Outputs above set radius
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
-
-    // log info about the circle
+    //Outputs below set radius
     circle.radius = 5;
     console.log("Raw circle information");
     circle.logInfo(false);
