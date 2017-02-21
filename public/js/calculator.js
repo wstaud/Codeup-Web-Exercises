5755306617e1b77a1c1da871e75b0a7fd9d5d085
xconstaud <xconstaud@gmail.com>
2017-02-21 12:48:10 -0600

    Update Calculator

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -27,10 +27,9 @@
 		}else if (buttonCount == 9) {
 			var button9 = buttonArray;
 		}
-		console.log(buttonCount);
+		console.log(buttonCount);  //debug
 	}
-	
-	//define special buttons
+	//define additional buttons
 	var buttonClear = document.getElementById("clear");
 	var buttonNegative = document.getElementById("isNegative");
 	var buttonPercentage = document.getElementById("percentage");
@@ -39,71 +38,120 @@
 	var buttonSubtract = document.getElementById("subtract");
 	var buttonAdd = document.getElementById("add");
 	var buttonEquals = document.getElementById("equals");
+	var buttonDecimal = document.getElementById("decimal");
 
 	//define the screen
 	var display = document.getElementById("display");
 	var centerHand = document.getElementById("centerHand");
-
 	var displayInput = document.getElementById("displayInput");
 
 
+	//Other Variables
+	displayInput.value = 0.0;   //Defualt Display Variable
+	var num1;  					//First number to be calculated
+	var num2;					//Second number to be calculated
+	var opperator;				//User picked operator to be applied to num1 and num2
+	var finished = false;		//Locks the equals button after equation is complete and displayed when true
+	// var total;				//May be used later to revert to a total system vs display system
 
+
+	//Function to display user inputed numbers on display
 	var changeMain = function (event) {
-		if (displayInput.value == 0) {
+		if (displayInput.value == 0.0) {
 			displayInput.value = this.innerHTML;
 		}else {
 			displayInput.value = displayInput.value + this.innerHTML;
 		}
 		
 	}
-	//clears the calculator
-	var clear = function (event) {
-		displayInput.value = 0;
+	//Changes positive number to negative and vise versa
+	var toNegative = function (event) {
+		if (displayInput.value > 0) {
+			displayInput.value *= -1;
+		}else if (displayInput.value < 0) {
+			displayInput.value *= 1;
+		}
 	}
 
-	var num1;
-	var num2;
-	var opperator;
+	//allows user to input a decimal
+	var decimal = function (event) {
+		var display = displayInput.value;
+		if (display.indexOf(".") > -1) {
+			display = display.split('.').join('');
+			display = display += ".";
+			displayInput.value = display;
+		}else {
+			displayInput.value += ".";
+		}
+	}
+	//Turns number into percentage of 100% (ex 100 = 1, 10 = .1, 1 = .01)
+	var percentage = function (event) {
+		displayInput.value *= .01;
+	}
+
+
+
+	//Function to clear the calculator, sets it back to zero (may be reverted to total later) and sets finished to false to allow equals again
+	var clear = function (event) {
+		displayInput.value = 0.0;
+		finished = false;
+	}
+
+	//Assigns num1 to what is on the display and assigns the opperator, sets up for num2 input.
 	var assignOpperator = function (event) {
 			if (this.innerHTML == "+") {
 				opperator = "add";
-				num1 = parseInt(displayInput.value);
-				displayInput.value = 0;
+				num1 = parseFloat(displayInput.value);
+				displayInput.value = "";
 			}else if (this.innerHTML == "-") {
 				opperator = "subtract";
-				num1 = parseInt(displayInput.value);
-				displayInput.value = 0;
+				num1 = parseFloat(displayInput.value);
+				displayInput.value = "";
 			}else if (this.innerHTML == "x") {
 				opperator = "multiply";
-				num1 = parseInt(displayInput.value);
-				displayInput.value = 0;
+				num1 = parseFloat(displayInput.value);
+				displayInput.value = "";
 			}else if (this.innerHTML == "/") {
 				opperator = "divide";
-				num1 = parseInt(displayInput.value);
-				displayInput.value = 0;
+				num1 = parseFloat(displayInput.value);
+				displayInput.value = "";
 			}
-			console.log(num1);
-			console.log(opperator);  //debug
+			console.log(num1);			//debug
+			console.log(opperator);  	//debug
 	}
 
+
+	//Assigns num2 to display, equates the problem and sets finished to true to prevent additional equals 
 	var complete = function (event) {
-		if (opperator == "add") {
-			num2 = parseInt(displayInput.value);
-			displayInput.value = (num1 + num2);
-		}else if (opperator == "subtract") {
-			num2 = parseInt(displayInput.value);
-			displayInput.value = (num1 - num2);
-		}else if (opperator == "multiply") {
-			num2 = parseInt(displayInput.value);
-			displayInput.value = (num1 * num2);
-		}else if (opperator == "divide") {
-			num2 = parseInt(displayInput.value);
-			displayInput.value = (num1 / num2);
+		if (finished == false) {
+			if (opperator == "add") {
+				num2 = parseFloat(displayInput.value);
+				displayInput.value = (num1 + num2);
+				finished = true;
+			}else if (opperator == "subtract") {
+				num2 = parseFloat(displayInput.value);
+				displayInput.value = (num1 - num2);
+				finished = true;
+			}else if (opperator == "multiply") {
+				num2 = parseFloat(displayInput.value);
+				displayInput.value = (num1 * num2);
+				finished = true;
+			}else if (opperator == "divide") {
+				num2 = parseFloat(displayInput.value);
+				displayInput.value = (num1 / num2);
+				finished = true;
+			}
+			
+		}else if (finished == true) {
+			displayInput.value = displayInput.value;
 		}
 		console.log(num2);  //debug
+		console.log(finished); //debug
 
 	}
 
+
+	//Number Button Listeners
 	button1.addEventListener("click", changeMain);
 	button2.addEventListener("click", changeMain);
 	button3.addEventListener("click", changeMain);
@@ -114,13 +162,19 @@
 	button8.addEventListener("click", changeMain);
 	button9.addEventListener("click", changeMain);
 	button0.addEventListener("click", changeMain);
-
+	//Additional Button listeners
 	buttonClear.addEventListener("click", clear);
 	buttonEquals.addEventListener("click", complete);
 	buttonAdd.addEventListener("click", assignOpperator);
 	buttonSubtract.addEventListener("click", assignOpperator);
 	buttonMultiply.addEventListener("click", assignOpperator);
 	buttonDivide.addEventListener("click", assignOpperator);
+	buttonDecimal.addEventListener("click", decimal);
+	buttonNegative.addEventListener("click", toNegative);
+	buttonPercentage.addEventListener("click", percentage);
+
+
+	
 
 
 
