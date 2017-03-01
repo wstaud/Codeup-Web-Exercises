c099c7bcf63858f3e6c2ca15036d3b5297892842
xconstaud <xconstaud@gmail.com>
2017-03-01 13:35:57 -0600

    Add weather_map

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -1,15 +1,31 @@
 (function() {
-    var APPID = "0d6974b5095c50379dabf3351c79f6e1";
+    var APPID = "7f8e3aa0aad113510e0c1eaafd1c17b8";
     var url = "http://api.openweathermap.org/data/2.5/forecast/daily";
-    var outputString ="";
     var output = $("#output");
+    var output2 = $("#sixDay");
+    var latLong = $("#cords");
+    var map = document.getElementById("maps");
+    var clouds = $("#clouds");
+    var deg = $("#deg");
+    var dt = $("#dt");
+    var humidity = $("#humidity");
+    var pressure = $("#preassure");
+    var speed = $("#speed");
+    var latLongString = "";
+
+
+    var outputString = "";
+
+    
+    
+
+
 
     var getWeather = function() {
         $.get(url, {
             APPID: APPID,
-            q: "San Antonio",
-            //lat: 29.423017,
-            //lon: -98.48527,
+            lat: lat,
+            lon: long,
             cnt: 3,  //days outputed up to 16
             units: "imperial"
 
@@ -18,20 +34,83 @@
         }).done(function(data) {
 
             data.list.forEach(function (data, index ,array) {
-                 outputString = data;
-            output.append(outputString);
-            console.log(outputString);
+                outputString += ("<div class='weatherBlock'>");
+                outputString += ("<div class='boxBanner'></div>");
+                outputString += ("<div class='temps space'>" + parseInt(data.temp.min) + "°/" + parseInt(data.temp.max) + "°</div>");
+                outputString += ("<div class='space'><img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'></div>");
+                outputString += ("<div class='space'>" + "<span class='bold'>" + data.weather[0].main + "</span> " + ": " + data.weather[0].description + "</div>");
+                outputString += ("<div class='space'>" + "<span class='bold'>Humidity: </span>" + data.humidity + "</div>");
+                outputString += ("<div class='space'>" + "<span class='bold'>Wind: </span>" + data.speed + "mph </div>");
+                outputString += ("<div class='space'>" + "<span class='bold'>Pressure: </span>" + data.pressure + "</div>");
+                outputString += ("</div>");
 
-                
+
+               
+                output.html(outputString);
+                console.log(data);
+
+                 
             });
-
-           
-
-            
 
         });
     };
+
+    var mapOptions = {
+            // Set the zoom level
+            zoom: 4,
+
+            // This sets the center of the map at our location
+            center: {
+                lat:  40.15555,
+                lng: -98.489602
+            }
+        };
+
+    var googleMap = new google.maps.Map(map, mapOptions);
+    var address = "170 Buffalo Pl, Cibolo, TX 78108";
+    var geocoder = new google.maps.Geocoder();
+    var myLatlng = new google.maps.LatLng(40.15555,-98.489602);
+    var lat = myLatlng.lat();
+    var long = myLatlng.lng();
+    var newLatLong = "";
+    //first lat/long run:
+    latLong.html("Lat: " + parseFloat(lat).toFixed(2) + "," + " Long: " + parseFloat(long).toFixed(2));
+    
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: googleMap,
+        draggable:true,
+        title:"Drag me!"
+       
+    });
+
+    marker.addListener('mouseup', function() {
+        myLatlng = marker.getPosition();
+        lat = myLatlng.lat();
+        long = myLatlng.lng();
+        latLongString = "";
+        latLongString += "Lat: "+ parseFloat(lat).toFixed(2) + ", Long: " + parseFloat(long).toFixed(2);
+        latLong.html(latLongString);
+        latLongString = "";
+        outputString = "";
+        getWeather();
+
+
+
+          
+        });
+
+    
+    
+
+
+
+
+
     getWeather();
+
+    
+
 
 
 })();
